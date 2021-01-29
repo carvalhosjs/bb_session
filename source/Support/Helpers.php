46780adfdee2a51766c6fdaed748265b30692204
medtradev <informatica@medtra.com.br>
2021-01-29 11:30:05 -0300

    ok

---
+++ source/Support/Helpers.php
@@ -0,0 +1,23 @@
+<?php
+function bb_session_request_limit(string $key, int $limit = 5, int $seconds = 60): bool
+{
+    $session = new \BBSession\Core\Session();
+    if ($session->has($key) && $session->$key->time >= time() && $session->$key->requests < $limit) {
+        $session->set($key, [
+            "time" => time() + $seconds,
+            "requests" => $session->$key->requests + 1
+        ]);
+        return false;
+    }
+
+    if ($session->has($key) && $session->$key->time >= time() && $session->$key->requests >= $limit) {
+        return true;
+    }
+
+    $session->set($key, [
+        "time" => time() + $seconds,
+        "requests" => 1
+    ]);
+
+    return false;
+}
